5da296bb81f566ce5672c621f82be056199fd1aa
Andrusco Marian <andrusco7marian@gmail.com>
2024-07-17 12:11:46 +0300

    Error fix in sidebar

--- src/components/Navbar/Sidenavbar.tsx
+++ src/components/Navbar/Sidenavbar.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import MobileNavbar from "./MobileNavbar";
-import DesktopNavbar from "./DesktopNavbar";
-import { useResize } from "../../utility/Utility";
+// import DesktopNavbar from "./DesktopNavbar";
+// import { useResize } from "../../utility/Utility";
 
 const Sidenavbar = () => {
   const [showTherapies, setShowTherapies] = useState<boolean>(false);
-  const { mobileNavbar } = useResize();
+  // const { mobileNavbar } = useResize();
   const [showBtnsMobile, setShowBtnsMobile] = useState<boolean>(false);
 
   return (
